c0eff0243f8bc8b6381b9df36bbb1fda90b553bf
jamesmcgill <jamesmcgill@users.noreply.github.com>
2019-11-29 21:53:26 +0100

    feat: destroy enemies with player shot

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Destroy(gameObject);
+        Destroy(other.gameObject);
+    }
+}
